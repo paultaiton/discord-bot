3a032f0c0d13c04e8b24f1dc38a86de43c77fa22
Paul Aiton <paul@aiton.info>
2021-02-20 19:58:47 -0700

    wip

--- discord-bot/__main__.py
+++ discord-bot/__main__.py
@@ -4,6 +4,7 @@
 '''
 import sys
 import discord
+from discord.ext import commands
 import argparse
 
 
@@ -14,22 +15,33 @@ def main(args=None):
     parser.add_argument("-t", "--token", action="store", help="Discord authentication token for bot account.")
     arguments = parser.parse_args()
 
-    discord_client = discord.Client()
+    # discord_client = discord.Client()
+    intents = discord.Intents.default()
+    bot = commands.Bot(command_prefix='$', description='Description Text Sample Paul', intents=intents)
+
     discord_token = arguments.token
 
-    @discord_client.event
+    @bot.event
     async def on_ready():
-        print('We have logged in as {0.user}'.format(discord_client))
+        print('We have logged in as {0.user}'.format(bot))
 
-    @discord_client.event
-    async def on_message(message):
-        if message.author == discord_client.user:
-            return
+    # @discord_client.event
+    # async def on_message(message):
+    #   # if message.author == discord_client.user:
+    #       # return
 
-        if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
+    #   # if message.content.startswith('$hello'):
+    #       # await message.channel.send('Hello!')
 
-    discord_client.run(discord_token)
+    @bot.command()
+    async def list_games(ctx, name='list_games'):
+        await ctx.send('sample blah blah')
+
+    @bot.command()
+    async def hello(ctx):
+        await ctx.send('Hello World')
+
+    bot.run(discord_token)
 
 
 if __name__ == '__main__':
